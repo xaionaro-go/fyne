61bfd3b898c0d025fe9f9a5111b682471044c81d
Dmitrii Okunev <xaionaro@dx.center>
2024-10-12 21:23:40 +0100

    Recover panic "send on closed channel"

--- internal/driver/common/window.go
+++ internal/driver/common/window.go
@@ -25,6 +25,9 @@ func (w *Window) InitEventQueue() {
 // QueueEvent uses this method to queue up a callback that handles an event. This ensures
 // user interaction events for a given window are processed in order.
 func (w *Window) QueueEvent(fn func()) {
+	defer func() {
+		recover()
+	}()
 	w.eventQueue.In() <- fn
 }
 
